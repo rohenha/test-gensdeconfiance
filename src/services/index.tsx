08eb3a5bf84101d4f006768a4590f3fe92053da0
rohenha <romain.breton.pro@gmail.com>
2020-09-23 08:59:58 +0200

    finish test

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 export async function getApiContent(email: string): Promise<[]> {
     try {
-        const response = await axios.get("http://localhost:5000/", {
+        const response = await axios.get("http://127.0.0.1:5000/", {
             params: {
                 email: email
             }
